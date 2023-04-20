9a3d45266180da9e5cae63d24505805fa4798725
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-04-20 11:54:37 +0530

    first commit

---
+++ e-comm/src/app/adminaddvender/adminaddvender.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminaddvender',
+  templateUrl: './adminaddvender.component.html',
+  styleUrls: ['./adminaddvender.component.css']
+})
+export class AdminaddvenderComponent {
+  
+
+}
